9af4325125bdcf6636fbf9b8d0375630925b7a30
Miguel Estrada Murillo <miguelestradam36@Miguels-MacBook-Pro.local>
2021-12-08 13:35:37 -0600

    Correcting small details

--- Hands-on Labs/lab-3/init_snowsql.sh
+++ Hands-on Labs/lab-3/init_snowsql.sh
@@ -1,4 +1,4 @@
 #Using direct SnowSQL
 snowsql -a ht48847.canada-central.azure -u miguel_estrada_sbx;
 #Using files in SnowSQL
-snowsql -a ht48847.canada-central.azure -u miguel_estrada_sbx -f script.sql;
+snowsql -a ht48847.canada-central.azure -u miguel_estrada_sbx -f file_snowsql.sql;
